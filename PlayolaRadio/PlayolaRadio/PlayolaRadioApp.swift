30c331107c25b34a0ee345b9c25dd42727335711
Brian Keane <lonesomewhistle@gmail.com>
2024-05-19 10:38:21 -0500

    first commit -- create project

---
+++ PlayolaRadio/PlayolaRadio/PlayolaRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayolaRadioApp.swift
+//  PlayolaRadio
+//
+//  Created by Brian D Keane on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayolaRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
