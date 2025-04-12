53b1373ad278115a85e02b91884b94d194763549
Brian Keane <lonesomewhistle@gmail.com>
2025-04-12 14:45:19 -0500

    RecordViewModel start and first test

---
+++ PlayolaRadio/Views/Pages/BroadcastBasePage/RecordingViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecordingViewTests.swift
+//  PlayolaRadioTests
+//
+//  Created by Brian D Keane on 4/11/25.
+//
+
+import Testing
+
+struct RecordingViewTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
