325a777275f2cbb0db044758b403078dca537d10
Brian Keane <lonesomewhistle@gmail.com>
2025-07-22 11:00:34 -0500

    add LocalListeningSession

---
+++ PlayolaRadio/Models/LocalListeningSession.swift
@@ -0,0 +1,13 @@
+//
+//  LocalListeningSession.swift
+//  PlayolaRadio
+//
+//  Created by Brian D Keane on 7/22/25.
+//
+import Foundation
+
+struct LocalListeningSession {
+  var id: UUID = UUID()
+  var startTime: Date!
+  var endTime: Date?
+}
